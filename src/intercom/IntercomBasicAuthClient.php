a6f206396f7066bc13f71c0a3b6bfe958a49d04e
Danny Fallon <danny@intercom.io>
2014-04-28 16:53:54 +0100

    Refactor BasicAuth to extend the abstract client

--- src/intercom/IntercomBasicAuthClient.php
+++ src/intercom/IntercomBasicAuthClient.php
@@ -1,13 +1,14 @@
 <?php
 namespace Intercom;
 
-use InvalidArgumentException;
 use Guzzle\Common\Collection;
 use Guzzle\Service\Client;
-use Guzzle\Service\Description\ServiceDescription;
 
-class IntercomBasicAuthClient extends Client
+class IntercomBasicAuthClient extends IntercomAbstractClient
 {
+    /** @var array The required config variables for this type of client */
+    private static $required = ['app_id', 'api_key', 'headers', 'service_description'];
+
     /**
      * Creates a Basic Auth Client with the supplied configuration options
      *
@@ -19,19 +20,12 @@ class IntercomBasicAuthClient extends Client
         $default = [
             'service_description' => __DIR__ . '/Service/config/intercom.json',
             'headers' => [
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/vnd.intercom.3+json'
+                'Content-Type' => self::DEFAULT_CONTENT_TYPE,
+                'Accept' => self::DEFAULT_ACCEPT_HEADER
             ]
         ];
 
-        $required = [
-            'app_id',
-            'api_key',
-            'headers',
-            'service_description'
-        ];
-
-        $config = Collection::fromConfig($config, $default, $required);
+        $config = Collection::fromConfig($config, $default, static::$required);
         $client = new self();
 
         $client->setDefaultOption('headers', $config->get('headers'));
@@ -45,21 +39,6 @@ class IntercomBasicAuthClient extends Client
         $client->setDescription(static::getServiceDescriptionFromFile($config->get('service_description')));
 
         return $client;
-    }
-
-    /**
-     * Loads the service description from the service description file
-     *
-     * @param string $description_file The service description file
-     * @return ServiceDescription
-     * @throws InvalidArgumentException If the description file doesn't exist or cannot be read
-     */
-    public function getServiceDescriptionFromFile($description_file) {
-        if (!file_exists($description_file) || !is_readable($description_file)) {
-            throw new InvalidArgumentException('Unable to read API definition schema');
-        }
-
-        return ServiceDescription::factory($description_file);
     }
 }
 
