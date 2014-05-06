b72fc4e0edc5c5756951da47220e002d0337bb1e
Danny Fallon <danny@intercom.io>
2014-05-06 13:36:27 +0100

    Move common logic to the abstract client class, fix non-throwing of 400s in error handler

--- src/intercom/IntercomBasicAuthClient.php
+++ src/intercom/IntercomBasicAuthClient.php
@@ -2,59 +2,33 @@
 namespace Intercom;
 
 use Guzzle\Common\Collection;
-use Guzzle\Common\Event;
 use Guzzle\Service\Client;
-use Guzzle\Http\Message\Request;
-use Intercom\Exception\IntercomException;
-
 
 class IntercomBasicAuthClient extends IntercomAbstractClient
 {
     /** @var array The required config variables for this type of client */
-    private static $required = ['app_id', 'api_key', 'headers', 'service_description'];
+    private static $required = [
+        'app_id',
+        'api_key',
+        'headers',
+        'service_description'
+    ];
 
     /**
-     * Creates a Basic Auth Client with the supplied configuration options
+     * Creates a basic auth client with the supplied configuration options
      *
      * @param array $config
      * @return Client|IntercomBasicAuthClient
      */
     public static function factory($config = [])
     {
-        $default = [
-            'service_description' => __DIR__ . '/Service/config/intercom.json',
-            'headers' => [
-                'Content-Type' => self::DEFAULT_CONTENT_TYPE,
-                'Accept' => self::DEFAULT_ACCEPT_HEADER
-            ]
-        ];
-
-        $config = Collection::fromConfig($config, $default, static::$required);
         $client = new self();
 
-        $client->setDefaultOption('headers', $config->get('headers'));
+        $config = Collection::fromConfig($config, $client->getDefaultConfig(), static::$required);
 
-        $client->setDefaultOption(
-            'auth',
-            [
-                $config->get('app_id'),
-                $config->get('api_key'),
-                'Basic'
-            ]
-        );
+        $client->configure($config);
 
-        $client->setDescription(static::getServiceDescriptionFromFile($config->get('service_description')));
-
-        $client->getEventDispatcher()->addListener('request.error', function(Event $event) {
-            // Stop other events from firing when you override 401 responses
-            $event->stopPropagation();
-
-            if ($event['response']->getStatusCode() > 400 && $event['response']->getStatusCode() < 600) {
-                $e = IntercomException::factory($event['request'], $event['response']);
-                $event['request']->setState(Request::STATE_ERROR, array('exception' => $e) + $event->toArray());
-                throw $e;
-            }
-        });
+        $client->setBasicAuth($config->get('app_id'), $config->get('api_key'));
 
         return $client;
     }
