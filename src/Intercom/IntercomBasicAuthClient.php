a537fc87bc97e96fec7462c401b300f88378ae0e
Jamie Osler <jamie@intercom.io>
2015-07-16 09:14:35 +0100

    version 1.1.1

--- src/Intercom/IntercomBasicAuthClient.php
+++ src/Intercom/IntercomBasicAuthClient.php
@@ -30,7 +30,7 @@ class IntercomBasicAuthClient extends IntercomAbstractClient
 
         $client->setBasicAuth($config->get('app_id'), $config->get('api_key'));
 
-        $client->setUserAgent('intercom-php/1.1.0', true);
+        $client->setUserAgent('intercom-php/1.1.1', true);
 
         return $client;
     }
