28fe11a532b5592c573c7d580426ffcb6ced8a54
Jean Baptiste Noblot <link00@hotmail.fr>
2016-08-03 11:44:22 +0200

    Support of scroll API (beta)

--- src/IntercomUsers.php
+++ src/IntercomUsers.php
@@ -21,4 +21,16 @@ class IntercomUsers {
   {
     return $this->client->get("users", $options);
   }
+  
+  /**
+  * Return a list of users
+  * @see https://developers.intercom.io/reference#iterating-over-all-users
+  * @param array $options if you didn't have scroll_param options must be null, else send an array with a key "scroll_param" with your scroll_param
+  * @return mixed principally an stdClass
+  * @throws \GuzzleHttp\Exception\GuzzleException
+  */
+  public function getUsersByScroll($options)
+  {
+    return $this->client->get("users/scroll", $options);
+  }
 }
