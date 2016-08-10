542d9b9e69e3442714e784f6dfedbb4b16af770d
amburke <alex.burke@intercom.io>
2016-08-09 22:12:44 -0700

    Add method for deleting a user
    There wasn't a very explicit way to delete a user available for customers. They seemed confused by this.

--- src/IntercomUsers.php
+++ src/IntercomUsers.php
@@ -21,4 +21,15 @@ class IntercomUsers {
   {
     return $this->client->get("users", $options);
   }
+  
+  public function deleteUser($id, $options = [])
+  {
+    $path = $this->userPath($id);
+    return $this->client->delete($path, $options);
+  }
+  
+  public function userPath($id)
+  {
+    return "users/" . $id;
+  }
 }
