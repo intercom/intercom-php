c6284617e04a273e5567b52ea7df1375608e6dc4
Bob Violier <bob@messagebird.com>
2017-04-14 11:22:34 +0200

    Add test for getting the path of an admin

--- test/IntercomAdminsTest.php
+++ test/IntercomAdminsTest.php
@@ -20,4 +20,12 @@ class IntercomAdminsTest extends PHPUnit_Framework_TestCase {
     $users = new IntercomAdmins($stub);
     $this->assertEquals('foo', $users->getAdmin(1));
   }
+
+  public function testAdminsGetPath()
+   {
+     $stub = $this->getMockBuilder('Intercom\IntercomClient')->disableOriginalConstructor()->getMock();
+
+     $users = new IntercomAdmins($stub);
+     $this->assertEquals('admins/1', $users->adminPath(1));
+   }
 }
