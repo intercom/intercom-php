f74810980ea04e0a15561b1c35d25f92cf685a40
Colin Longworth <colin.t.longworth@gmail.com>
2020-06-16 14:20:03 -0400

    Contact tag fix

--- src/IntercomContacts.php
+++ src/IntercomContacts.php
@@ -131,7 +131,7 @@ class IntercomContacts extends IntercomResource
     {
         $path = $this->contactPath($id);
         
-        return $this->client->post($path.'/tag', ['id' => $tag_id]);
+        return $this->client->post($path.'/tags', ['id' => $tag_id]);
     }
     
     /**
@@ -147,7 +147,7 @@ class IntercomContacts extends IntercomResource
     {
         $path = $this->contactPath($id);
         
-        return $this->client->delete($path.'/tag', ['id' => $tag_id]);
+        return $this->client->delete($path.'/tags', ['id' => $tag_id]);
     }
 
     /**
